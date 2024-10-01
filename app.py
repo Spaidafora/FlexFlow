724869d0db950c346b9da6aa728d56006b4862c1
spaidafora <spaidafora@gmail.com>
2024-09-30 19:00:35 -0700

    [BILL-66] can you see this commit?

--- app.py
+++ app.py
@@ -4,6 +4,5 @@ app = Flask(__name__)
 
 @app.route('/')
 def helloWorld():
-    return ('Hello, World! We are testing Linear with Github integration. We might use flask for this project !'
-            'For manual linking USING COMMIT MESSAGE: git commit -m your message [magic word BILL-11]')
+    return ('Hello, World!: git commit -m your message [magic word BILL-11]')
 
