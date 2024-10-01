9d3bcb651e9bccff4c65d8836856c1acfb194099
spaidafora <spaidafora@gmail.com>
2024-09-30 18:01:16 -0700

    BIL-11: commit message

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ app = Flask(__name__)
 @app.route('/')
 def helloWorld():
     return ('Hello, World! We are testing Linear with Github integration. We might use flask for this project !'
-            'For manual linking using COMMIT MESSAGE: git commit -m your message [magic word BILL-11]')
+            'For manual linking USING COMMIT MESSAGE: git commit -m your message [magic word BILL-11]')
 
